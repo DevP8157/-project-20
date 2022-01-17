f1b131e87cb58920fe958bf30f8509cdbf962908
DevP8157 <95598377+DevP8157@users.noreply.github.com>
2022-01-16 20:51:57 -0600

    Update sketch.js

--- project-template-device-compatible-main/sketch.js
+++ project-template-device-compatible-main/sketch.js
@@ -21,7 +21,7 @@ function preload(){
 function setup(){
   
 //create the canvas and adjust the window sizes to suit the device 
-
+createCanvas(windowWidth, windowHeight);
 path=createSprite(width/2,200);
 path.addImage(pathImg);
 path.velocityY = 4;
